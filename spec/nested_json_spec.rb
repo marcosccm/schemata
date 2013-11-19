fff71e428aeeaf15d5db44b9db9211595067e61b
Marcos Matos <marcosccm@gmail.com>
2013-11-19 00:59:21 -0200

    Integration spec for nested objects

---
+++ spec/nested_json_spec.rb
@@ -0,0 +1,48 @@
+require "json"
+require_relative "../lib/schemata"
+
+module Schemata
+  describe Schema do
+    let(:nested_json) do
+      {
+        "nested" => {
+          "name" => "marcos",
+          "items" => [1,2]
+        }
+      }.to_json
+    end
+
+    let(:nested_schema) do
+      {
+        "type" => "object",
+        "required" => true,
+        "properties" => {
+          "nested" => {
+            "type" => "object",
+            "required" => true,
+            "properties" => {
+              "name" => {
+                "type" => "string",
+                "required" => true
+              },
+              "items" => {
+                "type" => "array",
+                "required" => true,
+                "minItems" => 1,
+                "uniqueItems" => true,
+                "items" => {
+                  "type" => "integer",
+                  "required" => true
+                }
+              }
+            }
+          }
+        }
+      }.to_json
+    end
+
+    it "should be able to transform a json with nested objects into a schema" do
+      expect(Schema.generate nested_json).to eq(nested_schema)
+    end
+  end
+end
